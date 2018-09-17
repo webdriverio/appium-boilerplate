d58ddbb5423728ba2f0309a23ee37537b28ee7fc
Wim Selles <wim@wswebcreation.nl>
2018-09-17 21:32:50 +0200

    chore: update configs

--- config/wdio.ios.app.conf.js
+++ config/wdio.ios.app.conf.js
@@ -10,19 +10,10 @@ config.capabilities = [
         platformName: 'iOS',
         platformVersion: '11.1',
         orientation: 'PORTRAIT',
-        app: path.join(process.cwd(), './apps/iOS-NativeDemoApp-0.1.0.zip'),
+        app: path.join(process.cwd(), './apps/iOS-NativeDemoApp-0.2.0.zip'),
         noReset: true,
         newCommandTimeout: 240,
     },
 ];
-
-config.appium = {
-    args: {
-        address: '127.0.0.1',
-        commandTimeout: '11000',
-    },
-};
-
-config.port = 4723;
 
 exports.config = config;
