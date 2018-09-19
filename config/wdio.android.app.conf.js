5b2ac63d6ffec9098be828ecb0a9ab8d65e22cdf
wswebcreation <wim@wswebcreation.nl>
2018-09-19 11:48:58 +0200

    chore: update config docs

--- config/wdio.android.app.conf.js
+++ config/wdio.android.app.conf.js
@@ -4,14 +4,19 @@ const config = require('./wdio.shared.conf').config;
 // ============
 // Capabilities
 // ============
+// For all capabilities please check
+// http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
 config.capabilities = [
     {
+        // The defaults you need to have in your config
         automationName: 'UiAutomator2',
         deviceName: 'Nexus_5_7.1.1',
         platformName: 'Android',
         platformVersion: '7.1.1',
         orientation: 'PORTRAIT',
         app: path.join(process.cwd(), './apps/Android-NativeDemoApp-0.2.0.apk'),
+        // Read the reset strategies very well, they differ per platform, see
+        // http://appium.io/docs/en/writing-running-appium/other/reset-strategies/
         noReset: true,
         newCommandTimeout: 240,
     },
--- config/wdio.ios.app.conf.js
+++ config/wdio.ios.app.conf.js
@@ -4,13 +4,19 @@ const config = require('./wdio.shared.conf').config;
 // ============
 // Capabilities
 // ============
+// For all capabilities please check
+// http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
 config.capabilities = [
     {
+        // The defaults you need to have in your config
         deviceName: 'iPhone 6',
         platformName: 'iOS',
         platformVersion: '11.1',
         orientation: 'PORTRAIT',
+        // The path to the app
         app: path.join(process.cwd(), './apps/iOS-NativeDemoApp-0.2.0.zip'),
+        // Read the reset strategies very well, they differ per platform, see
+        // http://appium.io/docs/en/writing-running-appium/other/reset-strategies/
         noReset: true,
         newCommandTimeout: 240,
     },
