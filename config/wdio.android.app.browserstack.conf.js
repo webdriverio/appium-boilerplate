d59e6c225002b06d6e6ec3c10af1ef46d6c68aca
Hani Khan <hanikhan18@live.com>
2018-11-06 18:31:32 +0530

    Sample configuration for browserstack android

---
+++ config/wdio.android.app.browserstack.conf.js
@@ -0,0 +1,50 @@
+const browserstack = require('browserstack-local');
+
+exports.config = {
+  user: process.env.BROWSERSTACK_USERNAME || 'BROWSERSTACK_USERNAME',
+  key: process.env.BROWSERSTACK_ACCESS_KEY || 'BROWSERSTACK_ACC_KEY',
+
+  updateJob: false,
+  specs: [
+    './tests/specs/**/app*.spec.js'
+  ],
+  exclude: [],
+
+  capabilities: [{
+    automationName: 'UiAutomator2',
+    name: 'local_appium_test',
+    build: 'webdriverio-browserstack',
+    device: 'Google Pixel',
+    browserName: 'android',
+    app: process.env.BROWSERSTACK_APP_ID || 'bs://<hashed app-id>',
+    'browserstack.local': true,
+    'browserstack.debug': true
+  }],
+
+  logLevel: 'verbose',
+  coloredLogs: true,
+  screenshotPath: './errorShots/',
+  baseUrl: '',
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 90000,
+  connectionRetryCount: 3,
+
+  // Code to start browserstack local before start of test
+  onPrepare: function (config, capabilities) {
+    console.log("Connecting local");
+    return new Promise(function(resolve, reject){
+      exports.bs_local = new browserstack.Local();
+      exports.bs_local.start({'key': exports.config.key,'force':true }, function(error) {
+        if (error) return reject(error);
+        console.log('Connected. Now testing...');
+
+        resolve();
+      });
+    });
+  },
+
+  // Code to stop browserstack local after end of test
+  onComplete: function (capabilties, specs) {
+    exports.bs_local.stop(function() {});
+  }
+};
