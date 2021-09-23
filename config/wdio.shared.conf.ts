09040dbf6ca9e3ac2da1f5f101f89855d49ad028
Wim Selles <wim@wswebcreation.nl>
2021-09-23 17:54:28 +0200

    Update config/wdio.shared.conf.ts
    Co-authored-by: Christian Bromann <mail@christian-bromann.com>

--- config/wdio.shared.conf.ts
+++ config/wdio.shared.conf.ts
@@ -118,7 +118,7 @@ export const config: WebdriverIO.Config = {
          * NOTE: This has been increased for more stable Appium Native app
          * tests because they can take a bit longer.
          */
-        timeout: 180000,
+        timeout: 3 * 60 * 1000, // 3min
     },
     //
     // =====
