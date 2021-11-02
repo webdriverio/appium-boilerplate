69964ce8b09b1b1e6da5cedae05d393f029ba36b
Wim Selles <wim@wswebcreation.nl>
2021-11-02 11:29:32 +0100

    fix: add cli
    also update deps

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = {
     presets: [
         [ '@babel/preset-env', {
             targets: {
-                node: 12,
+                node: 14,
             },
         } ],
     ],
