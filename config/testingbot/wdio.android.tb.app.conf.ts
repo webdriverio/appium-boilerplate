7095d4420cc96f935eba2cc2c39e0afde57d9c3d
jochen <jochen@testingbot.com>
2023-11-08 16:40:35 +0100

    add TestingBot example

---
+++ config/testingbot/wdio.android.tb.app.conf.ts
@@ -0,0 +1,39 @@
+import { config } from '../wdio.shared.conf';
+
+// ============
+// Specs
+// ============
+config.specs = [
+    './tests/specs/**/app*.spec.js',
+];
+config.exclude = [
+    // Exclude this one because the test can only be executed on emulators/simulators
+    './tests/specs/**/app.biometric.login.spec.js',
+];
+
+// =============================
+// TestingBot specific config
+// =============================
+// User configuration
+config.user = process.env.TESTINGBOT_KEY || 'TESTINGBOT_KEY';
+config.key = process.env.TESTINGBOT_SECRET || 'TESTINGBOT_SECRET';
+// Use testingbot service
+config.services = ['testingbot'];
+
+// ============
+// Capabilities
+// ============
+// For all capabilities please check
+// http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
+config.capabilities = [
+    {
+        // Set URL of the application under test
+        app: 'https://testingbot.com/appium/sample.apk',
+
+        deviceName: 'Pixel 6',
+        platformName: 'Android',
+        version: '12.0',
+    },
+];
+
+exports.config = config;
