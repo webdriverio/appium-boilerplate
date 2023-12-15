2c78b03acb7c4def1deaca55735de1c5890b7a99
Christian Bromann <git@bromann.dev>
2023-12-14 16:49:17 -0800

    ci: Added browser support with actions - fixes #138

---
+++ config/wdio.browser.conf.ts
@@ -0,0 +1,31 @@
+import { config as baseConfig } from './wdio.shared.conf.js';
+
+export const config: WebdriverIO.Config = {
+    ...baseConfig,
+
+    // ============
+    // Specs
+    // ============
+    specs: ['../tests/specs/**/browser*.spec.ts'],
+
+    // ============
+    // Capabilities
+    // ============
+    // For all capabilities please check
+    // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
+    capabilities: [
+        {
+            browserName: 'chrome',
+        },
+        {
+            browserName: 'firefox',
+        },
+        {
+            browserName: 'edge',
+        },
+        {
+            maxInstances: 1,
+            browserName: 'safari',
+        }
+    ]
+};
