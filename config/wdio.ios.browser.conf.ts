450049b7dc1c1888422e93242b91dfd5a4eb3ed5
wswebcreation <wim@wswebcreation.nl>
2024-01-07 18:56:29 +0100

    chore: update mobile browser configs

--- config/wdio.ios.browser.conf.ts
+++ config/wdio.ios.browser.conf.ts
@@ -24,8 +24,13 @@ export const config: WebdriverIO.Config = {
             // For W3C the appium capabilities need to have an extension prefix
             // This is `appium:` for all Appium Capabilities which can be found here
             // http://appium.io/docs/en/writing-running-appium/caps/
-            'appium:deviceName': 'iPhone 12',
-            'appium:platformVersion': '14.5',
+
+            //
+            // NOTE: Change this name according to the Simulator you have created on your local machine
+            'appium:deviceName': 'iPhone 15',
+            //
+            // NOTE: Change this version according to the Simulator Version you have created on your local machine
+            'appium:platformVersion': '17.2',
             'appium:orientation': 'PORTRAIT',
             'appium:automationName': 'XCUITest',
             'appium:newCommandTimeout': 240,
