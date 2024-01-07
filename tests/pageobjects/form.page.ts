1965f10dac7e6ac140c3253dc2c4d71c87c96e6c
wswebcreation <wim@wswebcreation.nl>
2024-01-07 19:01:50 +0100

    fix: fix hide keyboard for iOS

--- tests/pageobjects/form.page.ts
+++ tests/pageobjects/form.page.ts
@@ -17,8 +17,19 @@ class FormPage extends Page {
         await this.username.setValue(username);
         await this.password.setValue(password);
         // only for mobile, if you test on a desktop browser `hideKeyboard` won't exist.
-        if (browser.hideKeyboard) {
-            await browser.hideKeyboard();
+        if (driver.isMobile) {
+            /**
+             * Normally we would hide the keyboard with this command `driver.hideKeyboard()`, but there is an issue for hiding the keyboard
+             * on iOS when using the command. You will get an error like below
+             *
+             *  Request failed with status 400 due to Error Domain=com.facebook.WebDriverAgent Code=1 "The keyboard on iPhone cannot be
+             *  dismissed because of a known XCTest issue. Try to dismiss it in the way supported by your application under test."
+             *  UserInfo={NSLocalizedDescription=The keyboard on iPhone cannot be dismissed because of a known XCTest issue. Try to dismiss
+             *  it in the way supported by your application under test.}
+             *
+             * That's why we click outside of the keyboard.
+             */
+            await $('h2').click();
         }
         await this.submitButton.click();
     }
