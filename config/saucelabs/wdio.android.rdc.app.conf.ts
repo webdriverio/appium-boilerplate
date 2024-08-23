34b105ffe2a29e0db30e0971c9f1ffe558c8cfa4
wswebcreation <wim@wswebcreation.nl>
2024-08-23 08:27:08 +0200

    chore: update to latest V9 version

--- config/saucelabs/wdio.android.rdc.app.conf.ts
+++ config/saucelabs/wdio.android.rdc.app.conf.ts
@@ -1,4 +1,4 @@
-import { config as baseConfig } from './wdio.shared.sauce.conf.js';
+import { config as baseConfig } from "./wdio.shared.sauce.conf.js";
 
 export const config: WebdriverIO.Config = {
     ...baseConfig,
@@ -6,12 +6,10 @@ export const config: WebdriverIO.Config = {
     // ============
     // Specs
     // ============
-    specs: [
-        '../tests/specs/**/app*.spec.ts',
-    ],
+    specs: ["../tests/specs/**/app*.spec.ts"],
     exclude: [
         // Exclude this one because the test can only be executed on emulators/simulators
-        '../tests/specs/**/app.biometric.login.spec.ts',
+        "../tests/specs/**/app.biometric.login.spec.ts",
     ],
 
     // ============
@@ -21,32 +19,32 @@ export const config: WebdriverIO.Config = {
     // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
     capabilities: [
         {
-            maxInstances: 5,
-            platformName: 'Android',
+            "wdio:maxInstances": 5,
+            platformName: "Android",
             // For more information about the supported Sauce Labs capabilities see:
             // https://wiki.saucelabs.com/display/DOCS/Appium+Capabilities+for+Real+Device+Testing
             // Sauce Labs RDC is not yet W3C compatible, that's why we use ts-ignore
             // @ts-ignore
-            'appium:deviceName': 'Samsung Galaxy S[8912].*',
-            'appium:automationName': 'UiAutomator2',
-            'appium:orientation': 'PORTRAIT',
-            'sauce:options': {
+            "appium:deviceName": "Samsung Galaxy S[8912].*",
+            "appium:automationName": "UiAutomator2",
+            "appium:orientation": "PORTRAIT",
+            "sauce:options": {
                 // Keep the device connected between tests so we don't need to wait for the cleaning process
-                cacheId: 'jsy1v49pn10',
+                cacheId: "jsy1v49pn10",
                 idleTimeout: 180,
                 // Add a name to the test
-                name: 'wdio-demo-app-test',
-                build: `WebdriverIO Native Demo app, Android Real Devices: ${new Date().getTime()}`
+                name: "wdio-demo-app-test",
+                build: `WebdriverIO Native Demo app, Android Real Devices: ${new Date().getTime()}`,
             },
 
-            'appium:newCommandTimeout': 240,
+            "appium:newCommandTimeout": 240,
             // The path to the app that has been uploaded to the Sauce Storage,
             // see https://wiki.saucelabs.com/display/DOCS/Application+Storage for more information
-            'appium:app': 'storage:filename=wdio-demo-app-android.apk',
-            'appium:appWaitActivity': 'com.wdiodemoapp.MainActivity',
+            "appium:app": "storage:filename=wdio-demo-app-android.apk",
+            "appium:appWaitActivity": "com.wdiodemoapp.MainActivity",
             // Always default the language to a language you prefer so you know the app language is always as expected
-            'appium:language': 'en',
-            'appium:locale': 'en',
+            "appium:language": "en",
+            "appium:locale": "en",
         },
     ],
 };
